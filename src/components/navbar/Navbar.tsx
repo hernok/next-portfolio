5df398d0330b7819e00c8ae097c47fe22e398d6d
hernok <hernok@hotmail.com>
2024-01-11 15:45:11 +0100

    pulling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -16,22 +16,24 @@ const Navbar = () => {
     }
   };
   return (
-    <nav className="bg-white select-none border-solid border-b-2 border-gray-300 py-8 flex justify-between px-3 md:px-10 dark:bg-slate-900 dark:border-0">
-      <h1 className="text-sm md:text-xl">Min Portefølje</h1>
-      <ul className="flex items-center">
-        <li>
-          <ThemeSwitch />
-        </li>
-        <li>
-          <button
-            className="text-sm md:text-lg dark:font-bold text-white bg-gradient-to-r from-gray-800 to-gray-700 px-3 py-1 rounded-lg sm:ml-2 md:ml-8 cursor-pointer dark:text-slate-900 dark:from-slate-100 dark:to-slate-200"
-            onClick={handleDownload}
-          >
-            Last ned CV
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav className="bg-white select-none border-solid border-b-2 border-gray-300 py-8 flex justify-between px-3 md:px-10 dark:bg-slate-900 dark:border-0">
+        <h1 className="text-sm md:text-xl">Min Portefølje</h1>
+        <ul className="flex items-center">
+          <li>
+            <ThemeSwitch />
+          </li>
+          <li>
+            <button
+              className="text-sm md:text-lg dark:font-bold text-white bg-gradient-to-r from-gray-800 to-gray-700 px-3 py-1 rounded-lg sm:ml-2 md:ml-8 cursor-pointer dark:text-slate-900 dark:from-slate-100 dark:to-slate-200"
+              onClick={handleDownload}
+            >
+              Last ned CV
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
