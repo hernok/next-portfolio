3b879bdf6919dd0b3f59361cf86f2c39be6b4314
hernok <hernok@hotmail.com>
2024-01-11 20:24:48 +0100

    fixed casing on import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "./providers";
-import Footer from "@/components/footer/footer";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -10,10 +10,10 @@ const AboutMe = () => {
       <p className="text-gray-800 text-sm py-2 md:text-xl dark:text-gray-200">
         Med erfaring i React, Next.js, og en nyoppdaget lidenskap for Tailwind,
         er jeg klar for å ta fatt på de reelle utfordringene som
-        utviklerverdenen byr på. Selv om jeg støtte på utfordringer, som en lav
-        karakter i JavaScript, har jeg opparbeidet meg erfaringer som har gjort
-        meg mer bestemt og dyktig. Jeg har også erfaring med SQLite3 samt
-        Node.js, CMS og nettstedsdeployment, demonstrert ved prosjektet{" "}
+        utviklerverdenen byr på. Jeg støtte på utfordringer med JavaScript i
+        starten av utdannelsen, men fikk god forståelse for det etter hvert. Jeg
+        har også erfaring med SQLite3 samt Node.js, CMS og nettstedsdeployment,
+        demonstrert ved prosjektet{" "}
         <Link
           className="text-purple-600 hover:text-purple-900 underline cursor-pointer dark:text-teal-400 dark:hover:text-purple-500"
           href="https://www.sagafarmann.com/"
@@ -23,8 +23,8 @@ const AboutMe = () => {
         .
       </p>
       <p className="text-gray-800 text-sm  py-2 md:text-xl dark:text-gray-200">
-        Jeg er på utkikk etter en rolle som frontend- eller webutvikler, som har
-        et godt arbeidsmiljø hvor samarbeid og kunnskapsdeling står i fokus. Jeg
+        Jeg er på utkikk etter en rolle som frontend- eller webutvikler, og et
+        godt arbeidsmiljø hvor samarbeid og kunnskapsdeling står i fokus. Jeg
         ser frem til muligheten til å bidra med mine tekniske ferdigheter og til
         å vokse som utvikler i et team som verdsetter kontinuerlig læring.
       </p>
